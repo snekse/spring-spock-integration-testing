466ef7d3c71710a356d36555e293f4779f399e99
Derek Eskens <derek@Dereks-MacBook-Pro.local>
2017-04-10 15:00:26 -0500

    Application bootstrap

---
+++ src/main/groovy/com/objectpartners/eskens/Application.groovy
@@ -0,0 +1,12 @@
+package com.objectpartners.eskens
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Application {
+
+	static void main(String[] args) {
+		SpringApplication.run Application, args
+	}
+}
