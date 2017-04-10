72416d3b537fcb7ddd0f1e572d77adeecc66070a
Derek Eskens <derek@Dereks-MacBook-Pro.local>
2017-04-10 18:06:54 -0500

    Make sure Person has an eq/hc for good testing comparisons

--- src/main/groovy/com/objectpartners/eskens/entities/Person.groovy
+++ src/main/groovy/com/objectpartners/eskens/entities/Person.groovy
@@ -1,5 +1,7 @@
 package com.objectpartners.eskens.entities
 
+import groovy.transform.Canonical
+
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
@@ -7,6 +9,7 @@ import javax.persistence.Table
 
 @Entity
 @Table
+@Canonical
 class Person {
 
     @Id @GeneratedValue
