bec00e1ca36882bb6ed7fba1e8b6d0c941b0db25
Derek Eskens <derek@Dereks-MacBook-Pro.local>
2017-04-10 18:36:42 -0500

    Add a @Service annotation to our service or Spring is going to have a tough time finding it

--- src/main/groovy/com/objectpartners/eskens/services/ExternalRankingService.groovy
+++ src/main/groovy/com/objectpartners/eskens/services/ExternalRankingService.groovy
@@ -1,11 +1,13 @@
 package com.objectpartners.eskens.services
 
 import com.objectpartners.eskens.entities.Person
+import org.springframework.stereotype.Service
 
 /**
  * This is to mimic calls to an external 3rd party service that you wouldn't want to test locally.
  * Created by derek on 4/10/17.
  */
+@Service
 class ExternalRankingService {
 
     @SuppressWarnings("GrMethodMayBeStatic")
