f46dd26ccf3450d6db065ccb47b675421a82630e
Derek Eskens <derek@Dereks-MacBook-Pro.local>
2017-04-10 17:40:47 -0500

    Working PersonRepo tests.
      Helps if you add a @Repository annotation to your repo and have the correct dependencies in your build file

--- src/main/groovy/com/objectpartners/eskens/repos/PersonRepo.groovy
+++ src/main/groovy/com/objectpartners/eskens/repos/PersonRepo.groovy
@@ -2,6 +2,10 @@ package com.objectpartners.eskens.repos
 
 import com.objectpartners.eskens.entities.Person
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface PersonRepo extends JpaRepository<Person, Long> {
+
+    List<Person> findByLastNameStartingWith(String lastName)
 }
--- src/test/groovy/com/objectpartners/eskens/repos/PersonRepoTest.groovy
+++ src/test/groovy/com/objectpartners/eskens/repos/PersonRepoTest.groovy
@@ -1,8 +1,8 @@
 package com.objectpartners.eskens.repos
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import spock.lang.Specification;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import spock.lang.Specification
 
 /**
  * Smoke tests to make sure repo works as expected
@@ -14,14 +14,25 @@ class PersonRepoTest extends Specification {
     @Autowired PersonRepo personRepo
 
     void "can select all from Person without blowing up"() {
-        when: 'doing a standard JPA select I from table'
+
+        when: 'doing a standard JPA select all from table'
         def result = personRepo.findAll()
 
-        then:
+        then: 'we do not blow up'
         noExceptionThrown()
 
-        then:
-        result.size() == 0
+        then: 'we find exactly 1 result from our seed data'
+        result.size() == 1
+    }
+
+    void 'can search by last name'() {
+
+        when: 'last name is expected in database start up script'
+        def persons = personRepo.findByLastNameStartingWith('Kir')
+
+        then: 'we find just capt james kirk'
+        persons.size() == 1
+        persons.first().firstName == 'James'
     }
 
 }
